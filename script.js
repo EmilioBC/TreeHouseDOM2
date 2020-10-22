648fcbce8d0bbb95497b14a92b5fd0af684707ec
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-22 23:30:58 +0100

    UpdateDOM1

--- script.js
+++ script.js
@@ -17,7 +17,23 @@ resetButton.addEventListener('click', () => {
 });
 
 labelInput.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
+    if(e.code == 'Enter') {
         myP.textContent = labelInput.value;
     }
 });
+
+
+const myList = document.getElementsByTagName('li');
+for(let i = 0; i < myList.length; i++) {
+    myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+for(let i = 0; i < errorNotPurple.length; i++) {
+    errorNotPurple[i].style.color = 'red';
+}
+
+const evens = document.querySelectorAll('li:nth-child(odd)');
+for(let i = 0; i <evens.length; i++) {
+    evens[i].style.color = 'lightgray';
+}
