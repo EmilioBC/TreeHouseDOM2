435f3b7be039ebc69e310af0571a742e936216a9
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-22 21:22:28 +0100

    UpdatingTags

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const resetButton = document.getElementById('reset');
 const myTextInput = document.getElementById('myTextInput');
+const labelInput = document.getElementById('labelInput');
+const myP = document.getElementsByTagName('p');
 
 
 myButton.addEventListener('click', () => {
@@ -12,4 +14,10 @@ myButton.addEventListener('click', () => {
 resetButton.addEventListener('click', () => {
     myHeading.style.color = 'black';
     myHeading.textContent = 'Javascript and the DOM';
+});
+
+labelInput.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        myP.textContent = labelInput.value;
+    }
 });
