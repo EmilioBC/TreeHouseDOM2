5fe038a17a12b3845c56a84af78f2aa643469849
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-22 19:56:14 +0100

    Update ID

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
+const resetButton = document.getElementById('reset');
 const myTextInput = document.getElementById('myTextInput');
 
+
 myButton.addEventListener('click', () => {
-    myHeading.style.color = myTextInput.value
+    myHeading.style.color = myTextInput.value;
     myHeading.textContent = myTextInput.value;
+});
 
-
+resetButton.addEventListener('click', () => {
+    myHeading.style.color = 'black';
+    myHeading.textContent = 'Javascript and the DOM';
 });
