e70cddf1878d0487ce2ffd093e749396ef310014
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-27 21:18:13 +0000

    DomTraverse

--- script.js
+++ script.js
@@ -9,19 +9,72 @@ const inputNew = document.getElementById('inputNew');
 const buttonNew = document.querySelector('button#buttonNew');
 const buttonRemove = document.getElementById('buttonRemove');
 const listDiv = document.getElementById('list');
+const listUl = listDiv.querySelector('ul');
+const lis = listUl.children;
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
+const listed = document.getElementsByTagName('li');
 
+firstListItem.style.backgroundColor = 'red';
+lastListItem.style.backgroundColor = 'yellow';
 
-listDiv.addEventListener('mouseover', (event) => {
-    if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toUpperCase();
+function attachListItemButtons (li) {
+    let up = document.createElement('button');
+    up.className = 'up';
+    up.textContent = 'up';
+    li.appendChild(up);
+    let down = document.createElement('button');
+    down.className = 'down';
+    down.textContent = 'down';
+    li.appendChild(down);
+    let remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'remove';
+    li.appendChild(remove);
 }
+
+for(let i = 0; i < lis.length; i++) {
+    attachListItemButtons(lis[i]);
+}
+
+listUl.addEventListener('click', (event) =>  {
+    if(event.target.tagName =='BUTTON') {
+        if(event.target.className == 'remove') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);  
+         }
+         if(event.target.className == 'up') {
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            if(prevLi) {
+            ul.insertBefore(li,prevLi);
+         }
+        }
+         if(event.target.className == 'down') {
+            let li = event.target.parentNode;
+            let nextLi = li.nextElementSibling;
+            let ul = li.parentNode;
+            if(nextLi) {
+            ul.insertBefore(nextLi, li);
+       }
+     }
+   }
 });
 
-listDiv.addEventListener('mouseout', (event) => {
-    if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toLowerCase();
-}
-});
+
+// listDiv.addEventListener('mouseover', (event) => {
+//     if (event.target.tagName == 'LI') {
+//     event.target.textContent = event.target.textContent.toUpperCase();
+// }
+// });
+
+// listDiv.addEventListener('mouseout', (event) => {
+//     if (event.target.tagName == 'LI') {
+//     event.target.textContent = event.target.textContent.toLowerCase();
+// }
+// });
 
 ///////////////// USEFUL FOR CHECKING TARGET EVENTS //////////////////
 // document.addEventListener('click', (event) => {
@@ -79,6 +132,7 @@ buttonNew.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = inputNew.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     inputNew.value = 'Type New Item...'
 })
